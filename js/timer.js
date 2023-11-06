b0a952711db0eaf5a6ca8528bd2fe83248c18d8c
Vovkanych Dmytro <dima.vovkanych24@gmail.com>
2023-11-06 17:33:47 +0200

    testing sw.js

--- js/timer.js
+++ js/timer.js
@@ -231,7 +231,7 @@ export function timer () {
         localStorage.setItem("timerInterval", timerInterval);
     }
 
-    navigator.serviceWorker.register('sw.js');
+    navigator.serviceWorker.register('../sw.js');
     Notification.requestPermission(function(result) {
     if (result === 'granted') {
         navigator.serviceWorker.ready.then(function(registration) {
